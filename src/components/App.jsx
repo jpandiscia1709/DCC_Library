2380088a40f9c9524f7a7a380ed97e16dbbd8bcd
jpandiscia1709 <jpangithub1709@gmail.com>
2021-10-05 17:07:58 -0400

    added book title and author display

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,32 @@ import TitleBar from './TitleBar/TitleBar';
 class App extends Component {
     constructor(props){
         super(props);
-        this.state = {};
+        this.books = [
+            {title:"Endurance", author:"Alfred Lansing"},
+            {title:"Black Hole Blues", author:"Janna Levin"},
+            {title:"The Magic of Believing", author:"Dr Claude Bristol"}
+        ];
+        this.state = {
+            bookNumber: 0
+        };
     }
 
     render() {
         return (
             <div className="container-fluid">
                 <TitleBar />
+                <div className="col-md-4">
+                    {/*Button here to move to previous book */}
+                </div>
+
+                <div className="col-md-4">
+                    {/*Display book with cover here */}
+                    <h1>{this.books[this.state.bookNumber].title}</h1>
+                    <h4>{this.books[this.state.bookNumber].author}</h4>
+                </div>
+                <div className="col-md-4">
+                    {/*Button here to move to next book */}
+                </div>
             </div>
         )
     }
